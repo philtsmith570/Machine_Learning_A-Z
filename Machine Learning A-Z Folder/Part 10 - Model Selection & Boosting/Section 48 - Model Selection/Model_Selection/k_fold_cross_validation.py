cdc598069e36cffa91a6d6040dda2322e681876d
philtsmith570 <phil.t.smith570@gmail.com>
2018-12-17 10:15:32 -0600

    Add files via upload
    Add model selection and XGBoost python projects

--- Machine Learning A-Z Folder/Part 10 - Model Selection & Boosting/Section 48 - Model Selection/Model_Selection/k_fold_cross_validation.py
+++ Machine Learning A-Z Folder/Part 10 - Model Selection & Boosting/Section 48 - Model Selection/Model_Selection/k_fold_cross_validation.py
@@ -22,7 +22,7 @@ X_test = sc.transform(X_test)
 
 # Fitting Kernel SVM to the Training set
 from sklearn.svm import SVC
-classifier = SVC(kernel = 'rbf', random_state = 0)
+classifier = SVC(kernel = 'rbf', random_state = 0, gamma='auto')
 classifier.fit(X_train, y_train)
 
 # Predicting the Test set results
@@ -31,13 +31,13 @@ y_pred = classifier.predict(X_test)
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
-print('cm: ', cm)
+print('Confusion Matrix:\n', cm)
 
 # Applying k-Fold Cross Validation
 from sklearn.model_selection import cross_val_score
 accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
 
-print('Accuries: ', accuracies)
+print('\nAccuries: ', accuracies)
 print('Accuries Mean: ', accuracies.mean())
 print('Std Accuries: ', accuracies.std())
 
